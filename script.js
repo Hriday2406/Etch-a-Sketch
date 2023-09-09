2ae9c6de690a7219eaa8bef5a0011090a76b6a3d
Hriday <hriday2602@gmail.com>
2023-09-10 02:43:03 +0530

    Complete page creation
    Add button for random color, eraser, clear grid and slider to change
    size of grid.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const container = document.querySelector('#container');
-let size = 16;
-createGrid(size);
-size++;
+function randomColor(min){
+    let r = Math.floor(Math.random()*(255 + 1)) + min;
+    let g = Math.floor(Math.random()*(255 + 1)) + min;
+    let b = Math.floor(Math.random()*(255 + 1)) + min;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
 
 function createGrid(size){
     for(let i=0; i<size; i++){
@@ -10,7 +12,7 @@ function createGrid(size){
             let gridBox = document.createElement('div');
             gridBox.classList.add('box');
             gridBox.addEventListener('mouseover', () => {
-                gridBox.style.backgroundColor = 'white';
+                gridBox.style.backgroundColor = randomColor(colored);
             });
             gridRow.append(gridBox);
         }
@@ -20,15 +22,40 @@ function createGrid(size){
 }
 
 function removeGrid(size){
-    for(let i=0; i<size; i++){
-        let row = document.querySelector('#container').firstChild;
-        container.removeChild(row);
-    }
+    for(let i=0; i<size; i++)
+        container.removeChild(document.querySelector('#container').firstChild);
 }
 
-const btnSize = document.querySelector('#btnSize');
-btnSize.addEventListener('click', () => {
+const container = document.querySelector('#container');
+let size = 16;
+colored = 255;
+createGrid(size);
+
+const btnColored = document.querySelector('#btnColored');
+btnColored.addEventListener('click', () => {
+    if(colored == 255)
+        colored = 0;
+    else
+        colored = 255;
+})
+
+const btnEraser = document.querySelector('#btnEraser');
+btnEraser.addEventListener('click', () => {
+    colored = -255;
+})
+
+const sliderValue = document.querySelector('#sliderValue');
+const slider = document.querySelector('#slider');
+slider.oninput = function() {
+    let temp = size;
+    size = this.value;
+    removeGrid(temp);
+    createGrid(size);
+    sliderValue.textContent = `Value = ${size}`;
+};
+
+const btnClear = document.querySelector('#btnClear');
+btnClear.addEventListener('click', () => {
     removeGrid(size);
-    size = +prompt("Enter size");
     createGrid(size);
 })
